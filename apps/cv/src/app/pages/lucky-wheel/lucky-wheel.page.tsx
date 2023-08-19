3ef7d3749d7380ed53aaece16db218c6ad9ce420
Alefers <antons.braims@softlabs.com>
2023-08-19 14:26:37 +0300

    lucky wheel sectors

--- apps/cv/src/app/pages/lucky-wheel/lucky-wheel.page.tsx
+++ apps/cv/src/app/pages/lucky-wheel/lucky-wheel.page.tsx
@@ -1,23 +1,65 @@
 import type { FC } from 'react';
 import s from './lucky-wheel.module.scss';
 import { __, cn } from '@repo/helpers';
+import { useState } from 'react';
 
+
+const getTan = (degree: number) =>
+  Math.tan((degree * Math.PI) / 180)
 
 const LuckyWheelPage: FC = () => {
-  const sectors = [0, 1, 2, 3, 4];
+  const [count, setCount] = useState(5);
+
+  const onCountChange = (val: number) => {
+    if (val > 1 && val < 20) {
+      setCount(val);
+    }
+  };
+
+  const sectors = Array.from(Array(count).keys());
+
+  const angle = 360 / count;
 
   const getSectorTransform = (idx: number): string => {
-    const angle = 360 / sectors.length;
+
     return [
       `rotate(${angle * idx}deg)`,
     ].join(', ');
-  }
+  };
+
+  const getPolygon = () => {
+    if (count < 4) {
+      const sideWidth = 100 - getTan(90 - angle / 2) * 100;
+      return `polygon(100% 0, 100% ${sideWidth}%, 50% 100%, 0 ${sideWidth}%, 0 0)`;
+    } else {
+      const halfWidth = getTan(angle / 2) * 50;
+      return `polygon(${50 - halfWidth}% 0, 50% 100%, ${50 + halfWidth}% 0)`;
+    }
+  };
 
   return (
     <div className={s.luckyWheel}>
       <h1 className="heading heading--h1">
         {__('Default_._Heading_._Lucky Wheel')}
       </h1>
+
+      <div className={s.count}>
+        <div
+          className={s.countButton}
+          onClick={() => onCountChange(count - 1)}
+        >
+          -
+        </div>
+        <div className={s.countCurrent}>
+          {count}
+        </div>
+        <div
+          className={s.countButton}
+          onClick={() => onCountChange(count + 1)}
+        >
+          +
+        </div>
+      </div>
       <div className={s.wheelWrapper}>
         <div className={s.wheel}>
           {sectors.map((sector) => (
@@ -28,9 +70,12 @@ const LuckyWheelPage: FC = () => {
               )}
               style={{
                 transform: getSectorTransform(sector),
+                clipPath: getPolygon(),
               }}
             >
-
+              <div>
+                {sector + 1}
+              </div>
             </div>
           ))}
         </div>
